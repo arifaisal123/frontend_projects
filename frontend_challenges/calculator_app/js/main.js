1e1206052bcc182f2a2a4fd48ac4bb4f3d24c3e9
arifaisal123 <arifaisal123@gmail.com>
2023-07-02 21:16:08 +0600

    Add styles for theme-2

--- frontend_challenges/calculator_app/js/main.js
+++ frontend_challenges/calculator_app/js/main.js
@@ -1,5 +1,12 @@
+const mainBody = document.getElementById("mainBody");
+const headerContainer = document.getElementById("headerContainer");
+const calcText = document.getElementById("calcText");
+const themeText = document.getElementById("themeText");
+const themeNum = document.getElementById("themeNum");
 const toggleButton = document.getElementById("toggleButton");
+const circleButton = document.getElementById("circleButton");
 const calculatorScreen = document.getElementById("calculatorScreen");
+const calcKeypad = document.getElementById("calcKeypad");
 const buttons = document.querySelectorAll(".numButton");
 const resetButton = document.getElementById("buttonReset");
 const deleteButton = document.getElementById("buttonDel");
@@ -9,10 +16,11 @@ const subtractionButton = document.getElementById("buttonSub");
 const divisionButton = document.getElementById("buttonDiv");
 const multiplicationButton = document.getElementById("buttonMult");
 const decimalButton = document.getElementById("buttonDec");
+const gridItems = document.querySelectorAll(".grid-item");
 let isDecimalAdded = false;
 let isCalculated = false;
+const attrText = document.getElementById("attrText");
 
-toggleButton.addEventListener("click", changeButtonPosition);
 
 buttons.forEach((button) => {
     button.addEventListener("click", updateScreen);
@@ -50,33 +58,116 @@ document.addEventListener("keydown", (event) => {
         case "0":
             updateScreen(event, source="0");
             break
+        case "+":
+            addNumbers();
+            break
+        case "-":
+            subtractNumbers();
+            break
+        case "*":
+            multiplyNumbers();
+            break
+        case "/":
+            divideNumbers();
+            break
+        case ".":
+            addDecimal();
+            break
+        case "Enter":
+            calculate();
+            break
+        case "Delete":
+            deleteDigits();
+            break
+        case " ":
+            resetScreen();
+            break
+        case "t":
+            toggleBehavior();
+            break
     }
 });
 
-resetButton.addEventListener("click", () => {
-    calculatorScreen.textContent = "0";
-    isDecimalAdded = false;
-});
 
-deleteButton.addEventListener("click", () => {
-    if (calculatorScreen.textContent.endsWith(".")) {
-        isDecimalAdded = false;
-    }
-    calculatorScreen.textContent = calculatorScreen.textContent.substring(0, calculatorScreen.textContent.length - 1)
-});
-
+toggleButton.addEventListener("click", toggleBehavior);
 addButton.addEventListener("click",  addNumbers);
+deleteButton.addEventListener("click", deleteDigits);
+resetButton.addEventListener("click", resetScreen);
 subtractionButton.addEventListener("click", subtractNumbers);
 divisionButton.addEventListener("click", divideNumbers);
 multiplicationButton.addEventListener("click", multiplyNumbers);
 decimalButton.addEventListener("click", addDecimal);
-
 equalButton.addEventListener("click", calculate);
 
-function changeButtonPosition() {
-    if (toggleButton.classList.contains("justify-content-start")) {
+
+function toggleBehavior() {
+    if (toggleButton.classList.contains("justify-content-start")) { 
         toggleButton.classList.remove("justify-content-start");
         toggleButton.classList.add("justify-content-center");
+
+        mainBody.classList.remove("bg-color-main");
+        mainBody.classList.add("bg-color-main-theme2");
+
+        headerContainer.classList.remove("bg-color-main");
+        headerContainer.classList.add("bg-color-main-theme2");
+
+        calcText.classList.remove("text-white");
+        calcText.classList.add("color-theme2-text");
+
+        themeText.classList.remove("text-white");
+        themeText.classList.add("color-theme2-text");
+
+        themeNum.classList.remove("text-white");
+        themeNum.classList.add("color-theme2-text");
+
+        toggleButton.classList.remove("bg-color-toggle");
+        toggleButton.classList.add("bg-color-toggle-theme2");
+
+        circleButton.classList.remove("bg-color-red");
+        circleButton.classList.add("bg-color-orange");
+
+        calcKeypad.classList.remove("color-screen-background");
+        calcKeypad.classList.add("color-keypad-background-theme2");
+
+        calculatorScreen.classList.remove("color-screen-background");
+        calculatorScreen.classList.remove("text-white");
+        calculatorScreen.classList.add("color-screen-background-theme2");
+        calculatorScreen.classList.add("color-theme2-text");
+
+        gridItems.forEach((item) => {
+            item.classList.remove("color-text-dark-grayish-blue", "color-key-background-1");
+            item.classList.add("color-theme2-text", "color-key-background-1-theme2", "button-box-shadow");
+        });
+
+        deleteButton.classList.remove("color-key-background-2");
+        deleteButton.classList.add("bg-color-cyan-theme2", "color-white", "box-shadow-cyan");
+        deleteButton.addEventListener("mouseenter", () => {
+            deleteButton.classList.add("bg-color-cyan-theme2-hover");
+        });
+        deleteButton.addEventListener("mouseleave", () => {
+            deleteButton.classList.remove("bg-color-cyan-theme2-hover");
+        });
+
+        resetButton.classList.remove("color-key-background-2");
+        resetButton.classList.add("bg-color-cyan-theme2", "color-white", "box-shadow-cyan");
+        resetButton.addEventListener("mouseenter", () => {
+            resetButton.classList.add("bg-color-cyan-theme2-hover");
+        });
+        resetButton.addEventListener("mouseleave", () => {
+            resetButton.classList.remove("bg-color-cyan-theme2-hover");
+        });
+
+        equalButton.classList.remove("color-key-background-3");
+        equalButton.classList.add("bg-color-orange-theme2", "color-white", "box-shadow-orange");
+        equalButton.addEventListener("mouseenter", () => {
+            equalButton.classList.add("bg-color-orange-theme2-hover");
+        });
+        equalButton.addEventListener("mouseleave", () => {
+            equalButton.classList.remove("bg-color-orange-theme2-hover");
+        });
+
+        attrText.classList.add("color-cyan");
+
     } else if (toggleButton.classList.contains("justify-content-center")) {
         toggleButton.classList.remove("justify-content-center");
         toggleButton.classList.add("justify-content-end");
@@ -107,7 +198,7 @@ function updateScreen(event, source="button") {
             alert("Calculator screen is limited to 16 digits!");
             console.log("Calculator screen digits limit of 16 exceeded.");
         } else {
-            calculatorScreen.textContent = calculatorScreen.textContent + buttonTextContent; 
+            calculatorScreen.textContent += buttonTextContent; 
         }
     }
 }
@@ -147,6 +238,18 @@ function addDecimal() {
     }
 }
 
+function deleteDigits() {
+    if (calculatorScreen.textContent.endsWith(".")) {
+        isDecimalAdded = false;
+    }
+    calculatorScreen.textContent = calculatorScreen.textContent.substring(0, calculatorScreen.textContent.length - 1);
+}
+
+function resetScreen() {
+    calculatorScreen.textContent = "0";
+    isDecimalAdded = false;
+}
+
 function calculate() {
     try {
         const result = eval(calculatorScreen.textContent);
@@ -157,3 +260,7 @@ function calculate() {
         calculatorScreen.textContent = "Error";
     }    
 }
+
+function formatNumberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
