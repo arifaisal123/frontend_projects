cc66542630aba720057f1dd45ea360ce33297e58
arifaisal123 <arifaisal123@gmail.com>
2023-07-02 10:00:26 +0600

    Add calculator JS functionality

--- frontend_challenges/calculator_app/js/main.js
+++ frontend_challenges/calculator_app/js/main.js
@@ -3,22 +3,40 @@ const calculatorScreen = document.getElementById("calculatorScreen");
 const buttons = document.querySelectorAll(".numButton");
 const resetButton = document.getElementById("buttonReset");
 const deleteButton = document.getElementById("buttonDel");
+const addButton = document.getElementById("buttonAdd");
+const equalButton = document.getElementById("buttonEqualTo");
+const subtractionButton = document.getElementById("buttonSub");
+const divisionButton = document.getElementById("buttonDiv");
+const multiplicationButton = document.getElementById("buttonMult");
+const decimalButton = document.getElementById("buttonDec");
+let isDecimalAdded = false;
+let isCalculated = false;
 
-toggleButton.addEventListener("click", () => {
-    changeButtonPosition();
-});
+toggleButton.addEventListener("click", changeButtonPosition);
 
 buttons.forEach((button) => {
     button.addEventListener("click", updateScreen);
 });
 
 resetButton.addEventListener("click", () => {
-    calculatorScreen.textContent = 0;
+    calculatorScreen.textContent = "0";
+    isDecimalAdded = false;
 });
 
 deleteButton.addEventListener("click", () => {
+    if (calculatorScreen.textContent.endsWith(".")) {
+        isDecimalAdded = false;
+    }
     calculatorScreen.textContent = calculatorScreen.textContent.substring(0, calculatorScreen.textContent.length - 1)
 });
+
+addButton.addEventListener("click",  addNumbers);
+subtractionButton.addEventListener("click", subtractNumbers);
+divisionButton.addEventListener("click", divideNumbers);
+multiplicationButton.addEventListener("click", multiplyNumbers);
+decimalButton.addEventListener("click", addDecimal);
+
+equalButton.addEventListener("click", calculate);
 
 function changeButtonPosition() {
     if (toggleButton.classList.contains("justify-content-start")) {
@@ -35,9 +53,66 @@ function changeButtonPosition() {
 
 function updateScreen(event) {
     const buttonTextContent = event.target.textContent;
-    if (calculatorScreen.textContent === "0"){
-        calculatorScreen.textContent = buttonTextContent;
-    } else {
-        calculatorScreen.textContent = calculatorScreen.textContent + buttonTextContent;   
+    if (isCalculated === true) {
+        calculatorScreen.textContent = "";
+        isCalculated = false;
+        isDecimalAdded = false;
     }
+
+    if ((calculatorScreen.textContent === "0") || (calculatorScreen.textContent === "")) {
+        calculatorScreen.textContent = buttonTextContent;
+    } else if (calculatorScreen.textContent.length < 17) {
+        if (calculatorScreen.textContent.length === 16) {
+            alert("Calculator screen is limited to 16 digits!");
+            console.log("Calculator screen digits limit of 16 exceeded.");
+        } else {
+            calculatorScreen.textContent = calculatorScreen.textContent + buttonTextContent; 
+        }
+    }
+}
+
+function addNumbers() {
+    if (calculatorScreen.textContent.length < 16) {
+        calculatorScreen.textContent += "+";
+        isDecimalAdded = false;
+    }
+}
+
+function subtractNumbers() {
+    if (calculatorScreen.textContent.length < 16) {
+        calculatorScreen.textContent += "-";
+        isDecimalAdded = false;
+    }
+}
+
+function divideNumbers() {
+    if (calculatorScreen.textContent.length < 16) {
+        calculatorScreen.textContent += "/";
+        isDecimalAdded = false;
+    }
+}
+
+function multiplyNumbers() {
+    if (calculatorScreen.textContent.length < 16) {
+        calculatorScreen.textContent += "*";
+        isDecimalAdded = false;
+    }
+}
+
+function addDecimal() {
+    if (!isDecimalAdded) {
+        calculatorScreen.textContent += ".";
+        isDecimalAdded = true;
+    }
+}
+
+function calculate() {
+    try {
+        const result = eval(calculatorScreen.textContent);
+        calculatorScreen.textContent = result;
+        isCalculated = true;
+        isDecimalAdded = false;
+    } catch (error) {
+        calculatorScreen.textContent = "Error";
+    }    
 }
