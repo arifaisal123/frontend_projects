5653fb2db3ddee19b56738eb7d7bd312f25adf30
arifaisal123 <arifaisal123@gmail.com>
2023-07-06 11:36:45 +0600

    Add mobile version

--- simon_game/game.js
+++ simon_game/game.js
@@ -3,6 +3,7 @@ let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
 let hasStarted = false;
+let isMobile = false;
 
 $(".btn").on("click", function() {
     const userChosenColor = $(this).attr("id");
@@ -12,10 +13,16 @@ $(".btn").on("click", function() {
     checkAnswer(userClickedPattern.length-1);
 });
 
-$(document).keydown(function(event) {
-    if (!hasStarted) {
-        nextSequence();
-        hasStarted = true;
+$(document).keydown(function() {
+    if (!isMobile) {
+        startGame();
+    }
+});
+
+$("#level-title-mobile").on("click", function() {
+    isMobileVersion();
+    if (isMobile) {
+        startGame();
     }
 });
 
@@ -29,7 +36,12 @@ function nextSequence() {
     playSound(randomChosenColor);
 
     level += 1;
-    $("#level-title").text(`Level ${level}`);
+    if (!isMobile) {
+        $("#level-title").text(`Level ${level}`);
+    } else {
+        $("#level-title-mobile").text(`Level ${level}`);
+    }
+    
 }
 
 function playSound(name) {
@@ -62,6 +74,13 @@ function checkAnswer(currentLevel) {
     }
 }
 
+function startGame() {
+    if (!hasStarted) {
+        nextSequence();
+        hasStarted = true;
+    }
+}
+
 function gameOver() {
     playSound("wrong");
 
@@ -71,7 +90,12 @@ function gameOver() {
         $("body").removeClass("game-over");
     }, 200);
 
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    if (!isMobile) {
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+    } else {
+        $("#level-title-mobile").text("Game Over, Click Here to Restart");
+    }
+    
     restartGame();
 }
 
@@ -79,4 +103,12 @@ function restartGame() {
   level = 0;
   gamePattern = [];
   hasStarted = false;
+}
+
+function isMobileVersion() {
+    if ($("#level-title").css("display") === "none") {
+        isMobile = true;
+    } else {
+        isMobile = false;
+    }
 }
