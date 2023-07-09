a10e94a77358baf3308e54154ae8262ca817d641
arifaisal123 <arifaisal123@gmail.com>
2023-07-09 11:13:17 +0600

    initial commit

---
+++ frontend_challenges/REST_countries/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import json
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+with open("data.json", encoding="UTF-8") as file:
+        data = json.load(file)
+
+@app.route("/")
+def index():
+    return render_template("index.html", data=data)
+
+@app.route("/<country>")
+def country_info(country):
+    country_data = None
+
+    for item in data:
+         if item["name"].lower() == country.lower():
+              country_data = item
+              break
+    
+    if country_data:
+         return render_template("country.html", country_data=country_data)
+    else:
+         return "Country not found."
